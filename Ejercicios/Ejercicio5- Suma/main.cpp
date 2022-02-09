91646476a669a95571a86b35275cb7e541e35bed
cllg0110 <carollopez0110@gmail.com>
2022-02-08 22:57:24 -0600

    Agregando suma

---
+++ Ejercicios/Ejercicio5- Suma/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+   int a = 0;
+   int b = 0;
+   int resultado = 0;
+
+   cout << "Ingrese el valor de a:";
+   cin >> a;
+   cout << "Ingrese el valor de b:";
+   cin >> b;
+
+   resultado = a + b;
+
+   cout << "Resultado:" << resultado << endl;
+    
+    return 0;
+}
