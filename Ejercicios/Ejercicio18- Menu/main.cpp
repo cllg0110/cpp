a3007a7e20414ce1ce891e81bbda0829f2d4fd12
cllg0110 <carollopez0110@gmail.com>
2022-02-13 21:35:06 -0600

    Agregando ejercicio menu

---
+++ Ejercicios/Ejercicio18- Menu/main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+
+using namespace std;
+
+int main(int argc, char** argv) {
+	
+	int opcion = 0;
+	
+	while(true) {
+		
+		cout << "****" << endl;
+		cout << "MENU" << endl;
+		cout << "****" << endl;
+		
+		cout <<  endl;
+		cout << "1 - Cafe y Granitas" << endl;
+		cout << "2 - Reposteria" << endl;
+		cout << "0 - Salir" << endl;
+		
+		cout << "Ingrese una opcion del menu: " ;
+		cin >> opcion;
+		
+		if(opcion == 1) {
+			system("cls");
+			cout << "Estas en el menu de cafe y granitas";
+			system ("pause");		
+		}
+		
+		if(opcion == 2) {
+			system("cls");
+			cout << "Estas en el menu de reposteria";
+			system ("pause");	
+		}
+			
+		if (opcion == 0) {
+			break;
+		}		
+    }
+	
+	cout << "Saliste del sistema" ;
+	
+	return 0;
+}
+
