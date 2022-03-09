9818f8ee6fb38b9522ea606a8c89fb73e17b6d62
cllg0110 <carollopez0110@gmail.com>
2022-03-09 01:38:11 -0600

    agregando ejercicio punto de venta

---
+++ Ejercicios/Examen1/examen1.cpp
@@ -0,0 +1,99 @@
+#include <iostream>
+using namespace std;
+
+int Modulus(int iN, int iMod) {
+    int iQ = (iN/iMod);
+    return iN - (iQ*iMod);
+}
+
+char GetChar(int iGenerator, char cBase, int iRange) {
+    return (cBase + Modulus(iGenerator, iRange));
+}
+
+int main() {
+    string contrasena;
+   
+    cout << "Eres un agente? ingresa tu contraseña ";
+    cin >> contrasena;
+   
+    if (contrasena == "smith") {
+        system("COLOR 02");
+
+        char caRow[80];
+        int j = 7;
+        int k = 2;
+        int l = 5;
+        int m = 1;
+        int ciclos = 0;
+
+        while (true) {
+            int i = 0;
+           
+            // caracteres aleatorios
+            while (i < 80) {
+                if (caRow[i] != ' ') {
+                    caRow[i] = GetChar(j + i*i, 33, 30);
+                }
+                std::cout << caRow[i];
+                ++i;
+            }
+            j = (j + 31);
+            k = (k + 17);
+            l = (l + 47);
+            m = (m + 67);
+            caRow[Modulus(j, 80)] = '-';
+            caRow[Modulus(k, 80)] = ' ';
+            caRow[Modulus(l, 80)] = '-';
+            caRow[Modulus(m, 80)] = ' ';
+           
+            // incremente el valor de 3000000 para retrasar el proceso
+            i = 0;
+            while (i < 300000) {
+                GetChar(1, 1, 1);
+                 ++i;
+            }
+           
+            ciclos ++;
+        }      
+       
+    }
+    int i = 0;
+        if (contrasena == "neo") {
+            system ("COLOR 04 ") ;
+            char caRow[10];
+           int j = 7;
+           int k = 2;
+           int l = 5;
+           int m = 1;
+
+           while (true) {
+              int i = 0;
+    
+            
+              while ( i < 10 ) {
+                   if (caRow[i] != ' ') {
+                    caRow[i] = GetChar(j + i*i, 33, 30);
+                }
+                std::cout << caRow[i];
+                i ++;
+                }
+            }
+            cout << endl;
+            cout << endl;
+            cout << "** El Sistema Fallo**";
+           
+            cout << endl;
+        }
+       
+    if (contrasena!= " smith " || contrasena!= "neo")
+    {
+        cout << endl;
+        cout << "acceso a la matrix denegado";
+       
+       
+        cout << endl;
+    }
+   
+
+    return 0;
+}
